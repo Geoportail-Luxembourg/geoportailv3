ae55190af9418de8d085c0c856abb56778a075f6
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2014-12-09 15:12:58 +0100

    Get rid of ngeo.html, use index.html instead

--- geoportailv3/views/views.py
+++
@@ -1,7 +0,0 @@
-from pyramid.view import view_config
-
-
-@view_config(route_name='ngeo',
-             renderer='geoportailv3:templates/ngeo.html')
-def ngeo(request):
-    return {'debug': 'debug' in request.params}
