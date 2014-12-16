bf7452bc396fda07673d0f7374b2d19e4a344353
Éric Lemoine <eric.lemoine@gmail.com>
2014-12-16 11:45:27 +0100

    Add a dbtunnel target to elemoine.mk

--- geoportailv3.mk
+++ geoportailv3.mk
@@ -1,5 +1,10 @@
-VARS_FILE ?= vars_geoportailv3.yaml
+ifdef VARS_FILE
+VARS_FILES += ${VARS_FILE} vars_geoportailv3.yaml
+else
+VARS_FILE = vars_geoportailv3.yaml
 VARS_FILES += ${VARS_FILE}
+endif
+
 TEMPLATE_EXCLUDE += LUX_alembic/script.py.mako
 
 include CONST_Makefile
