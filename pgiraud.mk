41366cc3358f55f562cf570c9437b86347a89189
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2014-12-17 09:23:13 +0100

    Add a dbtunnel target to pgiraud.mk

--- pgiraud.mk
+++ pgiraud.mk
@@ -1,3 +1,9 @@
 INSTANCE_ID = pgiraud
+VARS_FILE = vars_${INSTANCE_ID}.yaml
 
 include geoportailv3.mk
+
+.PHONY: dbtunnel
+dbtunnel:
+	@echo "Opening tunnel…"
+	ssh -N -L 9999:localhost:5432 luxembourg-geomapfish.infra.internal
