83a9b70ea4b1896e8984a66eaa789a87cada0321
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2015-01-06 15:36:50 +0100

    ngeo decorate the layers

--- geoportailv3/static/js/layerfactoryservice.js
+++ geoportailv3/static/js/layerfactoryservice.js
@@ -1,6 +1,7 @@
 goog.provide('app.LayerFactory');
 
 goog.require('app');
+goog.require('ngeo.DecorateLayer');
 goog.require('ol.layer.Image');
 goog.require('ol.layer.Tile');
 goog.require('ol.source.ImageWMS');
@@ -22,103 +23,112 @@ app.layerCache = {};
 
 /**
  * @private
- * @param {Object} node Catalog tree node.
- * @return {ol.layer.Layer} OpenLayers layer.
+ * @param {ngeo.DecorateLayer} ngeoDecorateLayer ngeo decorate layer service.
+ * @return {Function}
  */
-app.layerFactory_ = function(node) {
-  if (!('type' in node)) {
-    return null;
-  }
-  var type = node['type'];
-  if ([type, node['name']].join('_') in app.layerCache) {
-    return app.layerCache[type];
-  }
+app.layerFactory_ = function(ngeoDecorateLayer) {
+  /**
+   * @param {Object} node Catalog tree node.
+   * @return {ol.layer.Layer} OpenLayers layer.
+   */
+  return function(node) {
+    if (!('type' in node)) {
+      return null;
+    }
+    var type = node['type'];
+    if ([type, node['name']].join('_') in app.layerCache) {
+      return app.layerCache[type];
+    }
 
-  var layer;
+    var layer;
 
-  if (type.indexOf('WMS') != -1) {
-    layer = app.wmsLayerFactory_(node);
-  } else if (type == 'WMTS') {
-    layer = app.wmtsLayerFactory_(node);
-  } else {
-    return null;
-  }
-  app.layerCache[[type, node['name']].join('_')] = layer;
-  return layer;
-};
-
-
-/**
- * @private
- * @param {Object} node Catalog tree node.
- * @return {ol.layer.Layer} OpenLayers layer.
- */
-app.wmtsLayerFactory_ = function(node) {
-
-  var projection = ol.proj.get('EPSG:3857');
-
-  return new ol.layer.Tile({
-    source: new ol.source.WMTS({
-      url: 'http://wmts.geoportail.lu/mapproxy_4_v3/' +
-          'wmts/{Layer}/{TileMatrixSet}/{TileMatrix}/{TileCol}/{TileRow}.png',
-      layer: node['name'],
-      // FIXME should be replaced by node.matrixSet
-      // but there's a typo in the database for this attribute
-      matrixSet: 'GLOBAL_WEBMERCATOR',
-      requestEncoding: /** @type {ol.source.WMTSRequestEncoding} */ ('REST'),
-      format: 'image/png',
-      projection: projection,
-      tileGrid: new ol.tilegrid.WMTS({
-        origin: [-20037508.3428, 20037508.3428],
-        resolutions: [156543.033928, 78271.516964,
-          39135.758482, 19567.879241, 9783.9396205,
-          4891.96981025, 2445.98490513, 1222.99245256,
-          611.496226281, 305.748113141, 152.87405657,
-          76.4370282852, 38.2185141426, 19.1092570713,
-          9.55462853565, 4.77731426782, 2.38865713391,
-          1.19432856696, 0.597164283478, 0.298582141739],
-        matrixIds: [
-          0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-          11, 12, 13, 14, 15, 16, 17, 18, 19
-        ]
-      }),
-      style: 'default'
-    })
-  });
-};
+    if (type.indexOf('WMS') != -1) {
+      layer = wmsLayerFactory_(node);
+    } else if (type == 'WMTS') {
+      layer = wmtsLayerFactory_(node);
+    } else {
+      return null;
+    }
+    app.layerCache[[type, node['name']].join('_')] = layer;
+    layer.set('label', node['name']);
+    ngeoDecorateLayer(layer);
+    return layer;
+  };
 
 
-/**
- * @private
- * @param {Object} node Catalog tree node.
- * @return {ol.layer.Layer} OpenLayers layer.
- */
-app.wmsLayerFactory_ = function(node) {
-  var type = node['type'];
-  var layer;
-  var source;
-  if (type == 'internal WMS') {
-    source = new ol.source.ImageWMS({
-      // FIXME use the internalWmsUrl constant instead
-      url: 'http://devv3.geoportail.lu/main/wsgi/wms',
-      params: {
-        'LAYERS': node['name']
-      }
-    });
-  } else if (type == 'external WMS') {
-    source = new ol.source.ImageWMS({
-      url: node.url,
-      params: {
-        'LAYERS': node['name']
-      }
+  /**
+   * @private
+   * @param {Object} node Catalog tree node.
+   * @return {ol.layer.Layer} OpenLayers layer.
+   */
+  function wmtsLayerFactory_(node) {
+
+    var projection = ol.proj.get('EPSG:3857');
+
+    return new ol.layer.Tile({
+      source: new ol.source.WMTS({
+        url: 'http://wmts.geoportail.lu/mapproxy_4_v3/' +
+            'wmts/{Layer}/{TileMatrixSet}/{TileMatrix}/{TileCol}/{TileRow}.png',
+        layer: node['name'],
+        // FIXME should be replaced by node.matrixSet
+        // but there's a typo in the database for this attribute
+        matrixSet: 'GLOBAL_WEBMERCATOR',
+        requestEncoding: /** @type {ol.source.WMTSRequestEncoding} */ ('REST'),
+        format: 'image/png',
+        projection: projection,
+        tileGrid: new ol.tilegrid.WMTS({
+          origin: [-20037508.3428, 20037508.3428],
+          resolutions: [156543.033928, 78271.516964,
+            39135.758482, 19567.879241, 9783.9396205,
+            4891.96981025, 2445.98490513, 1222.99245256,
+            611.496226281, 305.748113141, 152.87405657,
+            76.4370282852, 38.2185141426, 19.1092570713,
+            9.55462853565, 4.77731426782, 2.38865713391,
+            1.19432856696, 0.597164283478, 0.298582141739],
+          matrixIds: [
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+            11, 12, 13, 14, 15, 16, 17, 18, 19
+          ]
+        }),
+        style: 'default'
+      })
     });
   }
 
-  layer = new ol.layer.Image({
-    source: /** @type {ol.source.Image} */ (source)
-  });
-  return layer;
+
+  /**
+   * @private
+   * @param {Object} node Catalog tree node.
+   * @return {ol.layer.Layer} OpenLayers layer.
+   */
+  function wmsLayerFactory_(node) {
+    var type = node['type'];
+    var layer;
+    var source;
+    if (type == 'internal WMS') {
+      source = new ol.source.ImageWMS({
+        // FIXME use the internalWmsUrl constant instead
+        url: 'http://devv3.geoportail.lu/main/wsgi/wms',
+        params: {
+          'LAYERS': node['name']
+        }
+      });
+    } else if (type == 'external WMS') {
+      source = new ol.source.ImageWMS({
+        url: node.url,
+        params: {
+          'LAYERS': node['name']
+        }
+      });
+    }
+
+    layer = new ol.layer.Image({
+      source: /** @type {ol.source.Image} */ (source)
+    });
+    return layer;
+  }
 };
 
 
-app.module.value('ngeoLayertreeLayerFactory', app.layerFactory_);
+
+app.module.factory('ngeoLayertreeLayerFactory', app.layerFactory_);
