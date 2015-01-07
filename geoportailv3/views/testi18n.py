f75c99b355ba98686118370bd24d2479da5b21dd
Patrick Weber <pweber@spatialbit.com>
2015-01-07 10:07:41 +0100

    simplified version of test i18n

--- geoportailv3/views/testi18n.py
+++ geoportailv3/views/testi18n.py
@@ -1,14 +1,12 @@
 from pyramid.view import view_config
 from pyramid.response import Response
-from pyramid.i18n import get_locale_name, TranslationStringFactory
-
+from pyramid.threadlocal import get_current_registry
 
 class I18n(object):
     def __init__(self,request):
-        self.request = request
-        self.lang = get_locale_name(request)
+            self.request = request
 
     @view_config(route_name='testi18n', renderer='testi18n.html')
     def testi18n(self):
         _ = self.request.translate
-        return {'title': _('title i18n')}
+        return {'title': _('title i18n'), 'language': self.request.locale_name }
