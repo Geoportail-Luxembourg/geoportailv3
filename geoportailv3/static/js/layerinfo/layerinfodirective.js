80c99d984942c9d87361f62f577e73852b708cc0
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2015-01-14 10:39:15 +0100

    Layerinfo directive & service

---
+++ geoportailv3/static/js/layerinfo/layerinfodirective.js
@@ -0,0 +1,49 @@
+goog.provide('app.layerinfoDirective');
+
+goog.require('app');
+goog.require('app.ShowLayerinfo');
+
+
+/**
+ * @return {angular.Directive} The Directive Definition Object.
+ * @ngInject
+ */
+app.layerinfoDirective = function(appLayerinfoTemplateUrl) {
+  return {
+    restrict: 'E',
+    scope: {
+      'layer': '=appLayerinfoLayer'
+    },
+    controller: 'AppLayerinfoController',
+    controllerAs: 'ctrl',
+    bindToController: true,
+    templateUrl: appLayerinfoTemplateUrl
+  };
+};
+
+app.module.directive('appLayerinfo', app.layerinfoDirective);
+
+
+/**
+ * @constructor
+ * @param {app.ShowLayerinfo} appShowLayerinfo app.ShowLayerinfo service.
+ * @ngInject
+ * @export
+ */
+app.LayerinfoController = function(appShowLayerinfo) {
+  /**
+   * @private
+   */
+  this.showLayerInfo_ = appShowLayerinfo;
+};
+
+
+/**
+ * @export
+ */
+app.LayerinfoController.prototype.getInfo = function() {
+  this.showLayerInfo_(this['layer']);
+};
+
+
+app.module.controller('AppLayerinfoController', app.LayerinfoController);
