25dbf81a3731e17d1e9979be2f50aa52a755e4a2
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2015-01-27 15:26:07 +0100

    Adding a make target to automatically build less files

--- pgiraud.mk
+++ pgiraud.mk
@@ -7,3 +7,8 @@ include geoportailv3.mk
 dbtunnel:
 	@echo "Opening tunnel…"
 	ssh -N -L 9999:localhost:5432 luxembourg-geomapfish.infra.internal
+
+.PHONY: watchless
+watchless:
+	@echo "Watching changes to less files…"
+	watchmedo shell-command --patterns="*.less" --recursive --command="make -f pgiraud.mk geoportailv3/static/build/build.css"
