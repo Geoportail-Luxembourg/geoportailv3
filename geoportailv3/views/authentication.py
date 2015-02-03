dc052434eed0a5364030b6137566331b4fbff857
rmichaelis <renaud.michaelis@gmail.com>
2015-02-03 16:45:35 +0100

    authenticates against ldap

---
+++ geoportailv3/views/authentication.py
@@ -0,0 +1,10 @@
+﻿# -*- coding: utf-8 -*-
+
+
+def ldap_user_validator(request, username, password):
+    from pyramid_ldap import get_ldap_connector
+    connector = get_ldap_connector(request)
+    data = connector.authenticate(username, password)
+    if data is not None:
+        return data[0]
+    return None
