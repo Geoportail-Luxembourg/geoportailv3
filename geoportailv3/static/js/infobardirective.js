75d26d125ce5d616863c2271594cb9f47ec61fb3
Patrick Weber <pweber@spatialbit.com>
2015-02-09 16:44:30 +0100

    added mouse position tracking to infobar, UTM zones not functional yet

--- geoportailv3/static/js/infobardirective.js
+++ geoportailv3/static/js/infobardirective.js
@@ -15,9 +15,8 @@
 goog.provide('app.infobarDirective');
 
 goog.require('app');
-goog.require('ngeo.controlDirective');
+goog.require('ol.control.MousePosition');
 goog.require('ol.control.Control');
-
 /**
  * @return {angular.Directive} The Directive Object Definition.
  * @ngInject
@@ -44,7 +43,63 @@ app.module.directive('appInfobar', app.infobarDirective);
  * @ngInject
  * @constructor
  */
-app.InfobarDirectiveController = function($document, $element) {
+app.InfobarDirectiveController = function($scope, $document) {
+    $scope.projectionOptions = [
+        {label: 'LUREF', value: 'EPSG:2169'},
+        {label: 'Long/Lat WGS84', value: 'EPSG:4326'},
+        {label: 'WGS84 UTM 32|31', value: 'EPSG:3263*'}
+    ];
+    $scope.projection = $scope.projectionOptions[0];
+    this.mouseposition = new ol.control.MousePosition({
+        className: 'custom-mouse-coordinates',
+        target: $document.find('.mouse-coordinates')[0],
+        projection: $scope.projection.value,
+        coordinateFormat: function(coord){
+            if ($scope.projection.value === "EPSG:3263*"){
+                var projection = ol.proj.get(_utmZoneCheck(coord));
+                this['map'].setProjection(projection);
+                return _coordFormat(coord, projection.getCode());
+            };
+            return _coordFormat(coord, $scope.projection.value)
+        }
+    });
+    this['map'].addControl(this.mouseposition);
+
+    function _utmZoneCheck(coord){
+        var lonlat = ol.proj.transform(coord, $scope.projection.value, "EPSG:4326");
+        if (Math.floor(lonlat.lon) >= 6){
+            return "EPSG:32632"
+        } else {
+            return "EPSG:32631";
+        }
+    }
+    function _coordFormat(coord, epsg_code){
+       switch(epsg_code){
+        case "EPSG:2169":
+            var template = '{x} E | {y} N';
+            return ol.coordinate.format(coord, template, 0);
+        case "EPSG:4326":
+            var hdms = ol.coordinate.toStringHDMS(coord);
+            var xhdms = hdms.split(" ").slice(0,4).join(" ");
+            var yhdms = hdms.split(" ").slice(4,8).join(" ");
+            var template = xhdms + ' ({x}) | ' +  yhdms +' ({y})';
+            return ol.coordinate.format(coord, template, 5);
+        case "EPSG:32632":
+            var template = '{x} | {y} (UTM32N)';
+            return ol.coordinate.format(coord, template, 0);
+        case "EPSG:32631":
+            var template = '{x} | {y} (UTM31N)';
+            return ol.coordinate.format(coord, template, 0);
+       }
+    }
+    $scope.switchProjection = function(){
+        var projection = ol.proj.get(this.projection.value);
+        var widget = this['ctrl'].mouseposition;
+        widget.setProjection(projection);
+        widget.setCoordinateFormat = function(coord) {
+            return _coordFormat(coord, this.projection.value);
+        }
+    }
 };
 
 
