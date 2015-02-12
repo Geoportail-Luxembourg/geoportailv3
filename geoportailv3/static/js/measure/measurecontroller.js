7f970e2b2bc6d426a04b43da8b9154e5cf4ec067
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2015-02-12 16:39:11 +0100

    Using the new ngeo measure interactions

--- geoportailv3/static/js/measure/measurecontroller.js
+++ geoportailv3/static/js/measure/measurecontroller.js
@@ -7,7 +7,9 @@ goog.provide('app.MeasureController');
 goog.require('app');
 goog.require('ngeo.DecorateInteraction');
 goog.require('ngeo.btngroupDirective');
-goog.require('ol.interaction.Draw');
+goog.require('ngeo.interaction.MeasureLength');
+goog.require('ngeo.interaction.MeasureArea');
+goog.require('ngeo.interaction.MeasureAzimut');
 goog.require('ol.source.Vector');
 
 
@@ -21,55 +23,54 @@ goog.require('ol.source.Vector');
  */
 app.MeasureController = function($scope, ngeoDecorateInteraction) {
 
-  this['map'] = $scope['mainCtrl']['map'];
-
-
-  /** @type {ol.source.Vector} */
-  var source = new ol.source.Vector();
-
-  /** @type {ol.layer.Vector} */
-  var vector = new ol.layer.Vector({
-    source: source,
-    style: new ol.style.Style({
+  var style = new ol.style.Style({
+    fill: new ol.style.Fill({
+      color: 'rgba(255, 255, 255, 0.2)'
+    }),
+    stroke: new ol.style.Stroke({
+      color: 'rgba(0, 0, 0, 0.5)',
+      lineDash: [10, 10],
+      width: 2
+    }),
+    image: new ol.style.Circle({
+      radius: 5,
+      stroke: new ol.style.Stroke({
+        color: 'rgba(0, 0, 0, 0.7)'
+      }),
       fill: new ol.style.Fill({
         color: 'rgba(255, 255, 255, 0.2)'
-      }),
-      stroke: new ol.style.Stroke({
-        color: '#ffcc33',
-        width: 2
-      }),
-      image: new ol.style.Circle({
-        radius: 7,
-        fill: new ol.style.Fill({
-          color: '#ffcc33'
-        })
       })
     })
   });
 
-  this['map'].addLayer(vector);
+  this['map'] = $scope['mainCtrl']['map'];
 
-  /** @type {ol.interaction.Draw} */
-  var measureDistance = new ol.interaction.Draw(
-      /** @type {olx.interaction.DrawOptions} */ ({
-        type: 'LineString',
-        source: source
-      }));
-  measureDistance.setActive(false);
-  ngeoDecorateInteraction(measureDistance);
-  this['map'].addInteraction(measureDistance);
-  this['measureDistance'] = measureDistance;
+  /** @type {ngeo.interaction.MeasureLength} */
+  var measureLength = new ngeo.interaction.MeasureLength({
+    sketchStyle: style
+  });
+  measureLength.setActive(false);
+  ngeoDecorateInteraction(measureLength);
+  this['map'].addInteraction(measureLength);
+  this['measureLength'] = measureLength;
 
-  /** @type {ol.interaction.Draw} */
-  var measureArea = new ol.interaction.Draw(
-      /** @type {olx.interaction.DrawOptions} */ ({
-        type: 'Polygon',
-        source: source
-      }));
+  /** @type {ngeo.interaction.MeasureArea} */
+  var measureArea = new ngeo.interaction.MeasureArea({
+    sketchStyle: style
+  });
   measureArea.setActive(false);
   ngeoDecorateInteraction(measureArea);
   this['map'].addInteraction(measureArea);
   this['measureArea'] = measureArea;
+
+  /** @type {ngeo.interaction.MeasureAzimut} */
+  var measureAzimut = new ngeo.interaction.MeasureAzimut({
+    sketchStyle: style
+  });
+  measureAzimut.setActive(false);
+  ngeoDecorateInteraction(measureAzimut);
+  this['map'].addInteraction(measureAzimut);
+  this['measureAzimut'] = measureAzimut;
 };
 
 app.module.controller('AppMeasureController', app.MeasureController);
