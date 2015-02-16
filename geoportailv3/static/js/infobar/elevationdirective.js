5b6cbcf24bdaf0595ba12844581f2af42a60f328
Patrick Weber <pweber@spatialbit.com>
2015-02-16 15:25:32 +0100

    implementation of the elevation widget

---
+++ geoportailv3/static/js/infobar/elevationdirective.js
@@ -0,0 +1,69 @@
+/**
+ * @fileoverview This file provides a "elevation" directive. This directive is
+ * used to insert Elevation information into the HTML page.
+ * Example:
+ *
+ * <app-elevation app-elevation-map="::mainCtrl.map"map></app-elevation>
+ *
+ * Note the use of the one-time binding operator (::) in the map expression.
+ * One-time binding is used because we know the map is not going to change
+ * during the lifetime of the application.
+ *
+ */
+goog.provide('app.elevationDirective');
+
+goog.require('app');
+goog.require('ngeo.Debounce');
+
+
+/**
+ * @return {angular.Directive} The Directive Object Definition.
+ * @ngInject
+ */
+app.elevationDirective = function() {
+  return {
+    restrict: 'E',
+    scope: {
+      'map': '=appElevationMap'
+    },
+    controller: 'AppElevationController',
+    controllerAs: 'ctrl',
+    bindToController: true,
+    template: '<span class="elevation" translate>' +
+                'Elevation: {{elevation}} m</span>'
+  };
+};
+
+
+app.module.directive('appElevation', app.elevationDirective);
+
+
+
+/**
+ * @ngInject
+ * @constructor
+ */
+app.ElevationDirectiveController = function($scope, $http, ngeoDebounce) {
+  this['map'].on('pointermove',
+      ngeoDebounce(function(e) {
+        var lonlat = /** @type {ol.Coordinate} */
+            (ol.proj.transform(e.coordinate,
+                   this.getView().getProjection(), 'EPSG:2169'));
+        $http({
+          url: 'raster',
+          method: 'GET',
+          params: {'lon': lonlat[0], 'lat': lonlat[1]}
+        }).
+            success(function(data) {
+              if (data['dhm'] > 0) {
+                $scope['elevation'] = parseInt(data['dhm'] / 100 , 0);
+              } else {
+                $scope['elevation'] = 'N/A';
+              }
+            });
+      }, 300, true));
+};
+
+
+app.module.controller('AppElevationController',
+    app.ElevationDirectiveController);
