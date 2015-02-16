6cea5a14d2ce428fcdf6bd825540456637385b90
Patrick Weber <pweber@spatialbit.com>
2015-02-16 15:28:18 +0100

    converted from dot to bracket notation to account for closure compiler

--- geoportailv3/static/js/infobar/projectionselectordirective.js
+++ geoportailv3/static/js/infobar/projectionselectordirective.js
@@ -43,24 +43,24 @@ app.module.directive('appProjectionselector', app.projectionselectorDirective);
  * @constructor
  */
 app.ProjectionselectorDirectiveController = function($scope, $document) {
-  $scope.projectionOptions = [
-    {label: 'LUREF', value: 'EPSG:2169'},
-    {label: 'Long/Lat WGS84', value: 'EPSG:4326'},
-    {label: 'WGS84 UTM 32|31', value: 'EPSG:3263*'}
+  $scope['projectionOptions'] = [
+    {'label': 'LUREF', 'value': 'EPSG:2169'},
+    {'label': 'Long/Lat WGS84', 'value': 'EPSG:4326'},
+    {'label': 'WGS84 UTM 32|31', 'value': 'EPSG:3263*'}
   ];
-  $scope.projection = $scope.projectionOptions[0];
+  $scope['projection'] = $scope['projectionOptions'][0];
   this.mouseposition = new ol.control.MousePosition({
     className: 'custom-mouse-coordinates',
     target: $document.find('.mouse-coordinates')[0],
-    projection: $scope.projection.value,
+    projection: $scope['projection']['value'],
     coordinateFormat: function(coord) {
-      return _coordFormat(coord, $scope.projection.value);
+      return _coordFormat(coord, $scope['projection']['value']);
     }
   });
   this['map'].addControl(this.mouseposition);
 
   function _utmZoneCheck(coord) {
-    var lonlat = /** @type {ol.Coordinate} */ (ol.proj.transform(coord, $scope['ctrl'].mouseposition.getProjection() , 'EPSG:4326'));
+    var lonlat = /** @type {ol.Coordinate} */ (ol.proj.transform(coord, $scope['ctrl']['mouseposition'].getProjection() , 'EPSG:4326'));
     if (Math.floor(lonlat[0]) >= 6) {
       return 'EPSG:32632';
     } else {
@@ -71,7 +71,7 @@ app.ProjectionselectorDirectiveController = function($scope, $document) {
   function _coordFormat(coord, epsg_code) {
     if (epsg_code === 'EPSG:3263*') {
       var projection = ol.proj.get(_utmZoneCheck(coord));
-      $scope['ctrl'].mouseposition.setProjection(projection);
+      $scope['ctrl']['mouseposition'].setProjection(projection);
       epsg_code = projection.getCode();
     }
     switch (epsg_code) {
@@ -91,15 +91,15 @@ app.ProjectionselectorDirectiveController = function($scope, $document) {
   };
 
   $scope.switchProjection = function() {
-    if (this.projection.value === 'EPSG:3263*') {
+    if (this['projection']['value'] === 'EPSG:3263*') {
       var projection = ol.proj.get('EPSG:32632');
     } else {
-      var projection = ol.proj.get(this.projection.value);
+      var projection = ol.proj.get(this['projection']['value']);
     }
-    var widget = this['ctrl'].mouseposition;
+    var widget = this['ctrl']['mouseposition'];
     widget.setProjection(projection);
     widget.setCoordinateFormat = function(coord) {
-      return _coordFormat(coord, $scope.projection.value);
+      return _coordFormat(coord, this['projection']['value']);
     };
   };
 };
