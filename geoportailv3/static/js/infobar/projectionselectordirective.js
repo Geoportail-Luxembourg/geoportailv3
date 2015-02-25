b5246c21ea2fc128d696e5e038037d84f4e7dbee
Patrick Weber <pweber@spatialbit.com>
2015-02-25 14:49:59 +0100

    use of goog.bind instead of angular bind one time binding for projection options

--- geoportailv3/static/js/infobar/projectionselectordirective.js
+++ geoportailv3/static/js/infobar/projectionselectordirective.js
@@ -54,11 +54,11 @@ app.ProjectionselectorDirectiveController = function($scope, $document) {
     className: 'custom-mouse-coordinates',
     target: $document.find('.mouse-coordinates')[0],
     projection: this.projection['value'],
-    coordinateFormat: /** @type {ol.CoordinateFormatType} */ (angular.bind(this, function(coord) {
+    coordinateFormat: /** @type {ol.CoordinateFormatType} */ (goog.bind(function(coord) {
       return this.coordFormat_(coord, this['projection']['value']);
-    }))
+    }, this))
   });
-  this.map.addControl(this.mouseposition) ;
+  this.map.addControl(this.mouseposition);
 };
 
 
@@ -66,58 +66,56 @@ app.ProjectionselectorDirectiveController = function($scope, $document) {
  * @private
  */
 app.ProjectionselectorDirectiveController.prototype.utmZoneCheck_ = function(coord) {
-    var lonlat = /** @type {ol.Coordinate} */ (ol.proj.transform(coord, this.mouseposition.getProjection() , 'EPSG:4326'));
-    if (Math.floor(lonlat[0]) >= 6) {
-      return 'EPSG:32632';
-    } else {
-      return 'EPSG:32631';
-    }
+  var lonlat = /** @type {ol.Coordinate} */ (ol.proj.transform(coord, this.mouseposition.getProjection() , 'EPSG:4326'));
+  return Math.floor(lonlat[0]) >= 6 ? 'EPSG:32632' : 'EPSG:32631';
 };
+
 
 /**
  * @private
  */
 app.ProjectionselectorDirectiveController.prototype.coordFormat_ = function(coord, epsg_code)
-{
-    if (epsg_code === 'EPSG:3263*') {
-        var projection = ol.proj.get(this.utmZoneCheck_(coord));
-        this.mouseposition.setProjection(projection);
-        epsg_code = projection.getCode();
-    }
-    console.log(epsg_code);
-    switch (epsg_code) {
-        case 'EPSG:2169':
-                   return ol.coordinate.format(coord, '{x} E | {y} N', 0);
-        case 'EPSG:4326':
-                   var hdms = ol.coordinate.toStringHDMS(coord);
-                   var yhdms = hdms.split(' ').slice(0, 4).join(' ');
-                   var xhdms = hdms.split(' ').slice(4, 8).join(' ');
-                   var template = xhdms + ' ({x}) | ' + yhdms + ' ({y})';
-                   return ol.coordinate.format(coord, template, 5);
-        case 'EPSG:32632':
-                   return ol.coordinate.format(coord, '{x} | {y} (UTM32N)', 0);
-        case 'EPSG:32631':
-                   return ol.coordinate.format(coord, '{x} | {y} (UTM31N)', 0);
-    }
+    {
+  if (epsg_code === 'EPSG:3263*') {
+    var projection = ol.proj.get(this.utmZoneCheck_(coord));
+    this.mouseposition.setProjection(projection);
+    epsg_code = projection.getCode();
+  }
+  switch (epsg_code) {
+    case 'EPSG:2169':
+      return ol.coordinate.format(coord, '{x} E | {y} N', 0);
+    case 'EPSG:4326':
+      var hdms = ol.coordinate.toStringHDMS(coord);
+      var yhdms = hdms.split(' ').slice(0, 4).join(' ');
+      var xhdms = hdms.split(' ').slice(4, 8).join(' ');
+      var template = xhdms + ' ({x}) | ' + yhdms + ' ({y})';
+      return ol.coordinate.format(coord, template, 5);
+    case 'EPSG:32632':
+      return ol.coordinate.format(coord, '{x} | {y} (UTM32N)', 0);
+    case 'EPSG:32631':
+      return ol.coordinate.format(coord, '{x} | {y} (UTM31N)', 0);
+  }
 };
+
+
 /**
  * @export
  */
 app.ProjectionselectorDirectiveController.prototype.switchProjection = function() {
-    var projection = null;
-    if (this['projection']['value'] === 'EPSG:3263*') {
-        projection = ol.proj.get('EPSG:32632');
-    } else {
-        projection = ol.proj.get(this['projection']['value']);
-    }
-    var widget = this.mouseposition;
-    console.log(projection);
-    widget.setProjection(projection);
-    widget.setCoordinateFormat(
-       /** @type {ol.CoordinateFormatType} */ (angular.bind(this, function(coord) {
-      return this.coordFormat_(coord, this['projection']['value']);
-    }))
-    );
+  var projection = null;
+  if (this['projection']['value'] === 'EPSG:3263*') {
+    projection = ol.proj.get('EPSG:32632');
+  } else {
+    projection = ol.proj.get(this['projection']['value']);
+  }
+  var widget = this.mouseposition;
+  console.log(projection);
+  widget.setProjection(projection);
+  widget.setCoordinateFormat(
+      /** @type {ol.CoordinateFormatType} */ (goog.bind(function(coord) {
+        return this.coordFormat_(coord, this['projection']['value']);
+      }, this))
+  );
 };
 
 app.module.controller('AppProjectionselectorController',
