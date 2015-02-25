f72486568cfdec837cda976167c2206dec827cbd
rmichaelis <renaud.michaelis@gmail.com>
2015-02-25 11:38:38 +0100

    don't write in remote response object

--- geoportailv3/static/js/layerinfo/layerinfoservice.js
+++ geoportailv3/static/js/layerinfo/layerinfoservice.js
@@ -14,16 +14,18 @@ goog.require('ngeo.CreatePopup');
 app.ShowLayerinfo;
 
 
+
 /**
  * @param {angular.$http} $http Angular $http service
  * @param {angular.$sce} $sce Angular $sce service
+ * @param {angular.Scope} $rootScope The rootScope provider.
  * @param {angularGettext.Catalog} gettextCatalog Gettext catalog.
  * @param {ngeo.CreatePopup} ngeoCreatePopup Ngeo popup factory service
- * @return {app.ShowLayerinfo} The show layer info function.
+ * @constructor
  * @ngInject
  */
-app.showLayerinfoFactory = function($http, $sce, gettextCatalog,
-    ngeoCreatePopup) {
+app.showLayerinfoFactory = function($http, $sce, $rootScope,
+    gettextCatalog, ngeoCreatePopup) {
 
   /**
    * @type {ngeo.Popup}
@@ -35,69 +37,89 @@ app.showLayerinfoFactory = function($http, $sce, gettextCatalog,
    */
   var promises_ = {};
 
-  return (
-      /**
-       * @param {ol.layer.Layer} layer The layer
-       */
-      function(layer) {
-        var title = /** @type {string} */ (layer.get('label'));
-        var localMetadata = /** @type {Object.<string>} */
-            (layer.get('metadata'));
-        var metadataUid = /** @type {string} */
-            (localMetadata['metadata_id']);
-        popup.setTitle(title);
-        var currentLanguage = /** @type {string} */
-            (gettextCatalog.currentLanguage);
-        var promiseKey = /** @type {string} */
-            (metadataUid + '##' + currentLanguage);
+  /**
+   * @type {ol.layer.Layer}
+   */
+  var currentLayer = null;
 
-        if (!(promiseKey in promises_)) {
-          promises_[promiseKey] = $http.jsonp(
-              'http://shop.geoportail.lu/Portail/inspire/webservices/getMD.jsp',
-              {params: {
-                'uid': metadataUid,
-                'lang': currentLanguage,
-                'cb': 'JSON_CALLBACK'
-              }}).then(
-                  angular.bind(this, function(resp) {
-                    var remoteMetadata = resp.data.root[0];
-                    remoteMetadata['uid'] = localMetadata['metadata_id'];
-                    if ('legend_name' in localMetadata) {
-                      remoteMetadata['legend_name'] =
-                      localMetadata['legend_name'];
-                      remoteMetadata['legend_url'] = $sce.trustAsResourceUrl(
-                          '//wiki.geoportail.lu/doku.php?id=' +
-                          gettextCatalog.currentLanguage + ':legend:' +
-                          remoteMetadata['legend_name'] + '&do=export_html'
-                      );
-                      remoteMetadata['has_legend'] = true;
-                    }else {
-                      remoteMetadata['has_legend'] = false;
-                    }
-                    remoteMetadata['is_error'] = false;
-                    remoteMetadata['is_short_desc'] = true;
-                    showPopup(remoteMetadata);
-                    return remoteMetadata;
-                  }));
-        }
+  $rootScope.$on('gettextLanguageChanged', function() {
+    if (!goog.isNull(currentLayer)) {
+      showLayerInfo(currentLayer);
+    }
+  });
 
-        promises_[promiseKey].then(
-            function(remoteMetadata) {
-              showPopup(remoteMetadata);
-            },
-            function(error) {
-              showPopup({'is_error': true});
-            });
+  return showLayerInfo;
 
-        /**
-         * @param {Object} content Object with metadata information.
-         */
-        function showPopup(content) {
-          popup.setContent(content);
-          popup.show();
-        };
+  /**
+   * @param {ol.layer.Layer} layer The layer
+   * @this {Object}
+   */
+  function showLayerInfo(layer) {
+    currentLayer = layer;
+    var title = /** @type {string} */ (layer.get('label'));
+    var localMetadata = /** @type {Object.<string>} */
+        (layer.get('metadata'));
+    var metadataUid = /** @type {string} */
+        (localMetadata['metadata_id']);
+    popup.setTitle(title);
+    var currentLanguage = /** @type {string} */
+        (gettextCatalog.currentLanguage);
+    var promiseKey = /** @type {string} */
+        (metadataUid + '##' + currentLanguage);
 
-      });
+    if (!(promiseKey in promises_)) {
+      promises_[promiseKey] = $http.jsonp(
+          'http://shop.geoportail.lu/Portail/inspire/webservices/getMD.jsp',
+          {params: {
+            'uid': metadataUid,
+            'lang': currentLanguage,
+            'cb': 'JSON_CALLBACK'
+          }}).then(
+              goog.bind(function(resp) {
+
+                var content = {
+                  'uid' : localMetadata['metadata_id'],
+                  'legendUrl' : null,
+                  'hasLegend' : false,
+                  'isError' : false,
+                  'isShortDesc' : true,
+                  'layerMetadata' : null
+                };
+
+                var remoteMetadata = resp.data.root[0];
+                content['layerMetadata'] = remoteMetadata;
+
+                if ('legend_name' in localMetadata) {
+                  content['legendUrl'] = $sce.trustAsResourceUrl(
+                      '//wiki.geoportail.lu/doku.php?id=' +
+                      gettextCatalog.currentLanguage + ':legend:' +
+                      localMetadata['legend_name'] + '&do=export_html'
+                  );
+                  content['hasLegend'] = true;
+                }
+
+                showPopup(content);
+                return content;
+              },this));
+    }
+
+    promises_[promiseKey].then(
+        function(content) {
+          showPopup(content);
+        },
+        function(error) {
+          showPopup({'isError': true});
+        });
+
+    /**
+     * @param {Object} content Object with metadata information.
+     */
+    function showPopup(content) {
+      popup.setContent(content);
+      popup.show();
+    };
+
+  }
 };
 
 app.module.service('appShowLayerinfo', app.showLayerinfoFactory);
