d1b24eb07ef3c2251ecb2577a45800362247d1fe
Jeff Konnen <jaykayone@gmail.com>
2015-03-05 16:29:07 +0100

    added some parameters for html5lib to produce valid HTML1

--- geoportailv3/views/preparelegend.py
+++ geoportailv3/views/preparelegend.py
@@ -3,6 +3,9 @@ from pyramid.response import Response
 from pyramid.httpexceptions import HTTPBadRequest
 import logging
 import urllib2
+from urlparse import urljoin
+import html5lib
+
 log = logging.getLogger(__name__)
 
 
@@ -22,8 +25,15 @@ class LegendPreparer(object):
 
         response = urllib2.urlopen(url)
         html = response.read()
-        html = html.replace("\"", "'")
-        html = html.replace("href='/", "href='http://wiki.geoportail.lu/")
-        html = html.replace("src='/", "src='http://wiki.geoportail.lu/")
+        document = html5lib.parse(html)
+        for img in document.iter("{http://www.w3.org/1999/xhtml}img"):
+                img.set("src", urljoin(url, img.get("src")))
+        for link in document.iter("{http://www.w3.org/1999/xhtml}link"):
+                link.set("href", urljoin(url, link.get("href")))
         headers = {"Content-Type": 'text/html'}
-        return Response(html, headers=headers)
+        output = html5lib.serialize(document,
+                quote_attr_values=True,
+                omit_optional_tags=False,
+                use_trailing_solidus=True,
+                quote_char=u"'")
+        return Response(output, headers=headers)
