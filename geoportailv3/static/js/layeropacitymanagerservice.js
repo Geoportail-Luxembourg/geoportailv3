e4ddadcfe9c53f79f22d72c8698718fe480c1b6b
Éric Lemoine <eric.lemoine@gmail.com>
2015-04-10 15:09:17 +0200

    Add a layer opacity manager
    This commit adds a layer opacity manager service responsible for resetting the opacity of layers added to the map.

---
+++ geoportailv3/static/js/layeropacitymanagerservice.js
@@ -0,0 +1,48 @@
+/**
+ * @fileoverview This file defines the "appLayerOpacityManager" service. This
+ * service (re-)sets the opacity of layers added to the map. The opacity is
+ * read from the layer's "start_opacity" metadata. If there's no metadata or
+ * if the "start_opacity" property doesn't exist then the layer opacity is
+ * reset to 1.
+ *
+ * Usage:
+ *
+ * appLayerOpacityManager.init(map);
+ *
+ */
+goog.provide('app.LayerOpacityManager');
+
+goog.require('app');
+goog.require('ol.CollectionEventType');
+
+
+
+/**
+ * @constructor
+ * @ngInject
+ */
+app.LayerOpacityManager = function() {
+};
+
+
+/**
+ * @param {ol.Map} map The map.
+ */
+app.LayerOpacityManager.prototype.init = function(map) {
+  var layers = map.getLayers();
+  goog.events.listen(layers, ol.CollectionEventType.ADD,
+      /**
+       * @param {ol.CollectionEventType} evt Collection event.
+       */
+      function(evt) {
+        var layer = /** @type {ol.layer.Layer} */ (evt.element);
+        var layerMetadata = layer.get('metadata');
+        var layerStartOpacity = goog.isDef(layerMetadata) &&
+            layerMetadata.hasOwnProperty('start_opacity') ?
+            +layerMetadata['start_opacity'] : 1;
+        layer.setOpacity(layerStartOpacity);
+      });
+};
+
+
+app.module.service('appLayerOpacityManager', app.LayerOpacityManager);
