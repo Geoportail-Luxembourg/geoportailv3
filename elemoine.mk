bfaf2e03811c31dcbedef335c5f02de6034c8122
Éric Lemoine <eric.lemoine@gmail.com>
2015-04-28 13:46:36 +0200

    Set variables for elemoine-specific print env

--- elemoine.mk
+++ elemoine.mk
@@ -1,6 +1,11 @@
 INSTANCE_ID = elemoine
 VARS_FILE = vars_${INSTANCE_ID}.yaml
-DISABLE_BUILD_RULES = apache print
+
+DISABLE_BUILD_RULES = apache
+
+PRINT_OUTPUT = tomcat
+TOMCAT_STOP_COMMAND =
+TOMCAT_START_COMMAND =
 
 include geoportailv3.mk
 
@@ -13,3 +18,8 @@ dbtunnel:
 watchless:
 	@echo "Watching changes to less files…"
 	nosier -p geoportailv3/static/less "make -f elemoine.mk geoportailv3/static/build/build.css"
+
+.PHONY: tomcat
+tomcat:
+	@echo "Running Tomcat…"
+	docker run --rm -it -p 8080:8080 -e Xmx=2048m -v /home/elemoine/src/geoportailv3/tomcat:/deployment maluuba/tomcat7
