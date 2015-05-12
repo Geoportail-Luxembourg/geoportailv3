4da6753b648c24ac722dbd8c187523d41d05211f
Patrick Weber <pweber@spatialbit.com>
2015-05-12 15:02:46 +0200

    sharing of long link through social and email

--- geoportailv3/static/js/share/sharedirective.js
+++ geoportailv3/static/js/share/sharedirective.js
@@ -41,17 +41,33 @@ app.module.directive('appShare', app.shareDirective);
  * @ngInject
  * @constructor
  * @param {angular.$window} $window
+ * @param {angularGettext.Catalog} gettextCatalog Gettext catalog.
  * @export
  */
-app.ShareDirectiveController = function($window) {
+app.ShareDirectiveController = function($window, gettextCatalog) {
+  /**
+   * @type {Object}
+   */
   this.services = {
     'facebook': {'url': 'https://www.facebook.com/sharer/sharer.php'},
     'twitter': {'url': 'https://www.twitter.com/intent/tweet'},
-    'googlePlus': {'url': 'https://www.plus.google.com/share'}
+    'googlePlus': {'url': 'https://plus.google.com/share'}
   };
+  /**
+   * @type {string}
+   */
   this.windowOptions =
     'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600';
+  /**
+   * @type {angular.$window}
+   * @private
+   */
   this.window_ = $window;
+  /**
+   * @type {angularGettext.Catalog}
+   * @private
+   */
+  this.translate_ = gettextCatalog;
 };
 
 /**
@@ -63,8 +79,15 @@ app.ShareDirectiveController.prototype.openShareLink = function(service) {
   var serviceUrl = goog.object.containsKey(this.services, service) ?
     this.services[service].url : false;
   if (serviceUrl) {
+    var googUri = new goog.Uri(serviceUrl);
+    googUri.setQueryData(goog.Uri.QueryData.createFromMap({
+      //twitter params
+      'text': this.window_.document.title,
+      'via': 'geoportal_lux',
+      'url': this.window_.location.href
+    }));
     var popup =
-      this.window_.open(serviceUrl, '_blank', this.windowOptions);
+      this.window_.open(googUri.toString(), '_blank', this.windowOptions);
     popup.focus();
   }
   return false;
@@ -75,7 +98,15 @@ app.ShareDirectiveController.prototype.openShareLink = function(service) {
  * @return {boolean}
  */
 app.ShareDirectiveController.prototype.openMailLink = function() {
-return false;
+  var googUri = new goog.Uri();
+  googUri.setScheme('mailto');
+  googUri.setQueryData(goog.Uri.QueryData.createFromMap({
+    'subject': this.window_.document.title +
+      this.translate_.getString(' - link from geoportail.lu'),
+    'body': this.window_.location.href
+  }));
+  this.window_.open(googUri.toString(), '_self', this.windowOptions);
+  return false;
 };
 
 app.module.controller('AppShareController', app.ShareDirectiveController);
