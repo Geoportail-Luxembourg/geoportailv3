2db9211ad4bb5e3860fc0baa8a4a1064c6a31b25
rmichaelis <renaud.michaelis@gmail.com>
2015-05-19 09:25:37 +0200

    get info from db and add additional info from python

--- geoportailv3/static/js/interrogation/interrogationdirective.js
+++ geoportailv3/static/js/interrogation/interrogationdirective.js
@@ -1,5 +1,6 @@
 goog.provide('app.interrogationDirective');
 
+
 /**
  * @param {string} appInterrogationTemplateUrl URL to directive template.
  * @return {angular.Directive} The Directive Definition Object.
@@ -18,49 +19,60 @@ app.interrogationDirective = function(appInterrogationTemplateUrl) {
     bindToController: true,
     templateUrl: appInterrogationTemplateUrl
   };
-}
+};
 app.module.directive('appInterrogation', app.interrogationDirective);
+
 
 
 /**
  * @constructor
  * @param {angular.$http} $http Angular $http service
- * @param {string} appInterrogationTemplateUrl Url to interrogation template
+ * @param {string} appInterrogationTemplatesPath Path to find the intterogation templates.
  * @export
  * @ngInject
  */
-app.InterrogationController = function($http, appInterrogationTemplateUrl) {
+app.InterrogationController = function($http, appInterrogationTemplatesPath) {
 
   this['content'] = [];
+  this['templatePath'] = appInterrogationTemplatesPath;
+
   var map = this['map'];
   var layers = map.getLayers().getArray();
 
   var layersList = [];
+  var box = [];
   map.on('singleclick', goog.bind(function(evt) {
     layersList = [];
-    for (var i = 0;i<layers.length;i++){
+    var buffer = 1;
+    var coordinate = (ol.proj.transform(evt.coordinate,
+        map.getView().getProjection(), 'EPSG:2169'));
+    box = [];
+    box.push(coordinate[0] - buffer);
+    box.push(coordinate[1] - buffer);
+    box.push(coordinate[0] + buffer);
+    box.push(coordinate[1] + buffer);
+
+    for (var i = 0; i < layers.length; i++) {
       var metadata = layers[i].get('metadata');
-      if (goog.isDefAndNotNull(metadata)){
-        if (metadata["is_queryable"] == "true"){
+      if (goog.isDefAndNotNull(metadata)) {
+        if (metadata['is_queryable'] == 'true') {
           layersList.push(layers[i].getSource().getLayer());
         }
       }
     }
 
     $http.get(
-      'http://workshop.geoportal.lu:8081/getfeatureinfo',
-      {params: {
-            'layers': layersList.join()
-      }}).then(
+        'http://workshop.geoportal.lu:8081/getfeatureinfo',
+        {params: {
+          'layers': layersList.join(),
+          'box': box.join()
+        }}).then(
         goog.bind(function(resp) {
-          console.log(evt.coordinate);
-          console.log(resp.data);
           this['content'] = resp.data;
-          this['infoOpen']=true;
+          this['infoOpen'] = true;
         },this));
   },this));
-
-}
+};
 
 
 app.module.controller('AppInterrogationController', app.InterrogationController);
--- geoportailv3/views/getfeatureinfo.py
+++ geoportailv3/views/getfeatureinfo.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 import sqlahelper
 import logging
-import json
-import geoalchemy2
+import sys
+import traceback
+import urllib2
 
+from urllib import urlencode
 from pyramid.view import view_config
-from sqlalchemy.sql import text
 from geoportailv3.models import LuxGetfeatureDefinition
 from pyramid.httpexceptions import HTTPBadRequest
+from geojson import loads as geojson_loads
 
 log = logging.getLogger(__name__)
+
 
 class Getfeatureinfo(object):
 
@@ -22,23 +25,184 @@ class Getfeatureinfo(object):
         layers = self.request.params.get('layers', None)
         if layers is None:
             return HTTPBadRequest()
-        DBSession = sqlahelper.get_session()
-        
-        try :
-            if layers is not None :
-                luxGetfeatureDefinitions = DBSession.query(
-                    LuxGetfeatureDefinition).filter(
-                        LuxGetfeatureDefinition.layer.in_(layers.split(','))).all()
-        except : 
+        box = self.request.params.get('box', None)
+        if box is None:
             return HTTPBadRequest()
 
-        #ST_GeomFromText('POLYGON((ulx uly, urx ury, llx llr, lrx lry, ulx uly))', <srid>)
+        dbsession = sqlahelper.get_session()
+
+        try:
+            luxgetfeaturedefinitions = []
+            if layers is not None:
+                for layer in layers.split(','):
+                    query = dbsession.query(
+                        LuxGetfeatureDefinition).filter(
+                            LuxGetfeatureDefinition.layer == layer
+                        )
+                    if self.request.user is not None:
+                        if query.filter(
+                                LuxGetfeatureDefinition.role == self.request.user.role.id
+                                ).count() > 0:
+                            luxgetfeaturedefinitions.append(query.filter(
+                                LuxGetfeatureDefinition.role == self.request.user.role.id
+                                ).first())
+                        else:
+                            luxgetfeaturedefinitions.append(query.filter(
+                                LuxGetfeatureDefinition.role == None
+                                ).first())
+                    else:
+                        luxgetfeaturedefinitions.append(query.filter(
+                            LuxGetfeatureDefinition.role == None
+                            ).first())
+        except:
+            traceback.print_exc(file=sys.stdout)
+            return HTTPBadRequest()
+
         results = []
-        for luxGetfeatureDefinition in luxGetfeatureDefinitions:
-            if luxGetfeatureDefinition.engine is not None :
-                engine = sqlahelper.get_engine(luxGetfeatureDefinition.engine)
-                res = engine.execute (luxGetfeatureDefinition.query)
+        for luxgetfeaturedefinition in luxgetfeaturedefinitions:
+            if luxgetfeaturedefinition.engine is not None and luxgetfeaturedefinition.query is not None:
+                engine = sqlahelper.get_engine(luxgetfeaturedefinition.engine)
+                coordinates = box.split(',')
+                res = engine.execute(
+                    luxgetfeaturedefinition.query
+                    % {'left': coordinates[0], 'bottom': coordinates[1],
+                       'right': coordinates[2], 'top': coordinates[3]})
                 rows = res.fetchall()
-                results.append ({luxGetfeatureDefinition.layer:[dict(r) for r in rows]})
+                if luxgetfeaturedefinition.additional_info_function is not None\
+                    and len(
+                        luxgetfeaturedefinition.additional_info_function
+                        ) > 0:
+                    results.append(
+                        {"template": luxgetfeaturedefinition.template,
+                         "layer": luxgetfeaturedefinition.layer,
+                         "values": eval(luxgetfeaturedefinition.
+                                        additional_info_function)})
+                else:
+                    results.append(
+                        {"template": luxgetfeaturedefinition.template,
+                         "layer": luxgetfeaturedefinition.layer,
+                         "values": [dict(r) for r in rows]})
+            if luxgetfeaturedefinition.rest_url is not None:
+                self._get_external_data(luxgetfeaturedefinition.rest_url, box)
+                results.append(
+                    {"template": luxgetfeaturedefinition.template,
+                     "layer": luxgetfeaturedefinition.layer,
+                     "values": []})
+        return results
+
+    def get_info_from_pf(self, rows):
+        results = []
+        import geoportailv3.PF
+        pf = geoportailv3.PF.PF()
+
+        for r in rows:
+            d = dict(r)
+            d['PF'] = dict(pf.get_detail(d['code_commu'],
+                                         d['code_secti'],
+                                         int(d['num_cadast']),
+                                         int(d['code_sup'])))
+            results.append(d)
 
         return results
+
+    def _get_external_data(self, url, bbox=None, featureid=None, cfg=None):
+        # ArcGIS Server REST API:
+        # http://help.arcgis.com/en/arcgisserver/10.0/apis/rest/query.html
+        # form example:
+        # http://ws.geoportail.lu/ArcGIS/rest/services/wassergis/waassergis_mxd/MapServer/45/query
+        #
+        # params "params_meaning" (params_type) [predefined_values]:
+        #
+        # f              "Format" (choice) [html kml pjson]
+        # geometry       "Filter Geometry" (string)
+        # geometryType   "Geometry Type" (choice) [esriGeometryEnvelope esriGeometryPoint esriGeometryPolyline esriGeometryPolygon esriGeometryMultipoint]
+        # inSR           "Input Spatial Reference (WKID)" (string)
+        # objectIds      "The object IDs of this layer / table to be queried" (string)
+        # outFields      "Return Fields (Comma Separated or *)" (string)
+        # outSR          "Output Spatial Reference (WKID)" (string)
+        # returnGeometry "Return Geometry" (bool true/false)
+        # spatialRel     "Spatial Relationship" (choice) [esriSpatialRelIntersects esriSpatialRelContains esriSpatialRelCrosses esriSpatialRelEnvelopeIntersects esriSpatialRelIndexIntersects esriSpatialRelOverlaps esriSpatialRelTouches esriSpatialRelWithin]
+        # text           "nom" (string)
+        # where          "Where" (string)
+        #
+        # example:
+        # http://ws.geoportail.lu/ArcGIS/rest/services/wassergis/waassergis_mxd/MapServer/45/query?text=&geometry=69000%2C124000%2C70000%2C125000&geometryType=esriGeometryEnvelope&inSR=2169&spatialRel=esriSpatialRelIntersects&where=&returnGeometry=true&outSR=&outFields=&f=pjson
+
+        body = {'f': 'pjson',
+                'geometry': '',
+                'geometryType': '',
+                'inSR': '2169',
+                'outSR': '',
+                'returnGeometry': 'true',
+                'spatialRel': '',
+                'text': '',
+                'where': '',
+                'outFields': '*',
+                'objectIds': ''}
+
+        if featureid is not None:
+            body['objectIds'] = featureid
+        elif bbox is not None:
+            body['geometry'] = bbox
+            body['geometryType'] = 'esriGeometryEnvelope'
+            body['spatialRel'] = 'esriSpatialRelIntersects'
+        else:
+            return []
+
+        # construct url for get request
+        separator = "?"
+        if url.find('?'):
+            separator = "&"
+        query = '%s%s%s' % (url, separator, urlencode(body))
+        try:
+            result = urllib2.urlopen(query, None, 15)
+            content = result.read()
+        except:
+            traceback.print_exc(file=sys.stdout)
+            return []
+
+        features = []
+        try:
+            esricoll = geojson_loads(content)
+        except:
+            raise
+        print esricoll
+        return esricoll
+        # find the esriFieldTypeOID field name (to recover the feature id)
+        esri_id_field_name = None
+        # if 'fields' in esricoll:
+        #    esri_id_field_name = self._get_esri_id_field_name(esricoll['fields'])
+        # parse esri json and build geosjon accordingly to the geometry type
+        if 'features' in esricoll:
+            for rawfeature in esricoll['features']:
+                f = ''
+                if rawfeature['geometry'] and 'x' in rawfeature['geometry'] and 'y' in rawfeature['geometry']:
+                    f = {
+                        'type': 'Feature',
+                        'geometry': {'type': 'Point',
+                                     'coordinates': [rawfeature['geometry']['x'], rawfeature['geometry']['y']]}
+                    }
+                elif rawfeature['geometry'] and 'x' in rawfeature['geometry'] and 'Y' in rawfeature['geometry']:
+                    f = {
+                        'type': 'Feature',
+                        'geometry': {'type': 'Point', 'coordinates': [rawfeature['geometry']['x'], rawfeature['geometry']['Y']]}
+                    }
+                elif rawfeature['geometry'] and 'paths' in rawfeature['geometry'] and len(rawfeature['geometry']['paths']) > 0:
+                    f = {
+                        'type': 'Feature',
+                        'geometry': {'type': 'MultiLineString', 'coordinates': rawfeature['geometry']['paths']}
+                    }
+                elif rawfeature['geometry'] and 'rings' in rawfeature['geometry'] and len(rawfeature['geometry']['rings']) > 0:
+                    f = {
+                        'type': 'Feature',
+                        'geometry': {'type': 'Polygon', 'coordinates': rawfeature['geometry']['rings']}
+                    }
+
+                if f != '':
+                    pass
+                    # store attribute in c for template generation
+                    # id = rawfeature['attributes']['_f_id_'] = self._get_feature_id(esri_id_field_name, rawfeature)
+                    # c.feature = rawfeature['attributes']
+                    # features.append(c.feature)
+
+        return features
