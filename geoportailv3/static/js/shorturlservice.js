3aaa4a7b6ab162048563ec8fb843e88a083044f8
Patrick Weber <pweber@spatialbit.com>
2015-06-03 14:18:09 +0200

    code improvements according to @elemoine

--- geoportailv3/static/js/shorturlservice.js
+++ geoportailv3/static/js/shorturlservice.js
@@ -24,27 +24,25 @@ app.GetShorturl;
 app.getShorturl_ = function($http, ngeoLocation, shorturlServiceUrl) {
   return (
       /**
-     * @param {ol.Coordinate=} opt_coordinate
-     * @return {!angular.$q.Promise} Promise providing the short URL.
-     */
+       * @param {ol.Coordinate=} opt_coordinate
+       * @return {!angular.$q.Promise} Promise providing the short URL.
+       */
       function getShorturl(opt_coordinate) {
-        var location = ngeoLocation;
-        if (opt_coordinate) {
-          location.updateParams({
+        if (goog.isDef(opt_coordinate)) {
+          ngeoLocation.updateParams({
             'X': Math.round(opt_coordinate[0]),
             'Y': Math.round(opt_coordinate[1])
           });
         }
-        var url = location.getUriString();
         return $http.get(shorturlServiceUrl, {
           params: {
-            'url': url
+            'url': ngeoLocation.getUriString()
           }
         }).then(
             /**
-         * @param {angular.$http.Response} resp Ajax response.
-         * @return {string} The short URL.
-         */
+           * @param {angular.$http.Response} resp Ajax response.
+           * @return {string} The short URL.
+           */
             function(resp) {
               return resp.data['short_url'];
             });
