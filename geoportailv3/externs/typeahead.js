6ee4b6ce85dc2a8c95be128690e654ccacf33e6c
Éric Lemoine <eric.lemoine@gmail.com>
2015-09-02 15:32:34 +0200

    Add extra extern definition for typeahead v0.10.5

---
+++ geoportailv3/externs/typeahead.js
@@ -0,0 +1,12 @@
+/**
+ * @fileoverview Extern definitions for typeahead.
+ * @externs
+ */
+
+
+/**
+ * displayKey was replaced by display in typeahead v0.11.1. So we need this
+ * extra extern definition so long that we use v0.10.5.
+ * @type {string|undefined}
+ */
+TypeaheadDataset.prototype.displayKey;
