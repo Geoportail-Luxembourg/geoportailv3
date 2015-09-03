236e63b5d81400bd2aea78c0d1f35e59ff068dcf
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2015-09-03 15:17:54 +0200

    Base code for the mymaps directive

---
+++ geoportailv3/static/js/mymaps/mymapsdirective.js
@@ -0,0 +1,43 @@
+/**
+ * @fileoverview This file provides a "mymaps" directive. This directive is
+ * used to insert a MyMaps block  into the HTML page.
+ * Example:
+ *
+ * <app-mymaps></app-mymaps>
+ *
+ */
+goog.provide('app.MymapsDirectiveController');
+goog.provide('app.mymapsDirective');
+
+goog.require('app');
+
+
+/**
+ * @return {angular.Directive} The Directive Object Definition.
+ * @param {string} appMymapsTemplateUrl
+ * @ngInject
+ */
+app.mymapsDirective = function(appMymapsTemplateUrl) {
+  return {
+    restrict: 'E',
+    scope: true,
+    controller: 'AppMymapsController',
+    controllerAs: 'ctrl',
+    bindToController: true,
+    templateUrl: appMymapsTemplateUrl
+  };
+};
+
+
+app.module.directive('appMymaps', app.mymapsDirective);
+
+
+
+/**
+ * @ngInject
+ * @constructor
+ * @export
+ */
+app.MymapsDirectiveController = function() {};
+
+app.module.controller('AppMymapsController', app.MymapsDirectiveController);
