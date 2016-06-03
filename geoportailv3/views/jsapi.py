670fe9cd917936e7b3dcc2113200840a98bca9cd
Antoine Abt <antoine@abt.im>
2016-06-03 09:48:07 +0200

    Add route `/api-example` to test loader

--- geoportailv3/views/jsapi.py
+++ geoportailv3/views/jsapi.py
@@ -8,3 +8,9 @@ log = logging.getLogger(__name__)
              renderer='geoportailv3:templates/api/apiv3loader.js')
 def apiloader(request):
     return {}
+
+
+@view_config(route_name='jsapiexample',
+             renderer='geoportailv3:templates/api/apiv3example.html')
+def apiexample(request):
+    return {}
