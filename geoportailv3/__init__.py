98e76614fe788d5299aa026ff1068a29db7c4e23
Antoine Abt <antoine@abt.im>
2016-06-03 09:48:07 +0200

    Add route `/api-example` to test loader

--- geoportailv3/__init__.py
+++ geoportailv3/__init__.py
@@ -247,4 +247,5 @@ def main(global_config, **settings):
     config.add_route('getremotetemplate', '/getremotetemplate')
     config.add_route('isthemeprivate', '/isthemeprivate')
     config.add_route('jsapiloader', '/apiv3loader.js')
+    config.add_route('jsapiexample', '/api-example')
     return config.make_wsgi_app()
