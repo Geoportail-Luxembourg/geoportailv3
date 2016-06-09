3e809cf4f7135afff215ca141b7baff1a3dbac0f
Antoine Abt <antoine@abt.im>
2016-06-09 16:42:17 +0200

    API to set map center, zoom & bg layer & overlays

--- jsapi/examples/simple.js
+++ jsapi/examples/simple.js
@@ -3,5 +3,10 @@ goog.provide('index');
 goog.require('lux');
 
 var map = new lux.Map({
-  target: 'mapContainer'
+  target      : 'mapContainer',
+  position    : [ 6.13, 49.61 ],
+  positionSrs : '4326',
+  zoom        : 14,
+  bgLayer     : 'streets_jpeg',
+  layers      : [ '543' ]
 });
