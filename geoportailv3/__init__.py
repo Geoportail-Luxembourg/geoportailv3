d710c050ef712896f09221265b358f9ee1f25dbe
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2016-06-23 09:50:38 +0200

    Consistent syntax for routes

--- geoportailv3/__init__.py
+++ geoportailv3/__init__.py
@@ -207,6 +207,16 @@ def main(global_config, **settings):
     # layer search routes
     config.add_route("layersearch", "/layersearch")
 
+    # jsapi routes
+    config.add_route(
+        'jsapiloader',
+        '/apiv3loader.js'
+    )
+    config.add_route(
+        'jsapiexample',
+        '/api-example'
+    )
+
     config.include('c2cgeoportal')
     config.include('pyramid_closure')
 
@@ -274,6 +284,4 @@ def main(global_config, **settings):
     config.add_route('getpoitemplate', '/getpoitemplate')
     config.add_route('getremotetemplate', '/getremotetemplate')
     config.add_route('isthemeprivate', '/isthemeprivate')
-    config.add_route('jsapiloader', '/apiv3loader.js')
-    config.add_route('jsapiexample', '/api-example')
     return config.make_wsgi_app()
