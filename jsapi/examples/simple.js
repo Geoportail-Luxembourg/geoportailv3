ff8d8121fc7a4a90a7301bce48c711f1ec1d26e0
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2016-06-23 15:52:38 +0200

    Use the new layers service

--- jsapi/examples/simple.js
+++ jsapi/examples/simple.js
@@ -1,12 +1,12 @@
 goog.provide('index');
 
-goog.require('lux');
+goog.require('lux.Map');
 
 var map = new lux.Map({
   target      : 'mapContainer',
   position    : [ 6.13, 49.61 ],
   positionSrs : '4326',
   zoom        : 14,
-  bgLayer     : 'streets_jpeg',
-  layers      : [ '543' ]
+  bgLayer     : 655,
+  layers      : [ '145' ]
 });
