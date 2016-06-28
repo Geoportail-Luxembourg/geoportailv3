355e02bd0b547339372be4ab8eb85a26d2166b32
Antoine Abt <antoine@abt.im>
2016-06-28 09:53:56 +0200

    Add Proj4 to build

--- jsapi/src/main.js
+++ jsapi/src/main.js
@@ -13,6 +13,7 @@ goog.require('ol.source.OSM');
 goog.require('ol.source.ImageWMS');
 goog.require('ol.source.WMTSRequestEncoding');
 
+proj4.defs('EPSG:2169','+proj=tmerc +lat_0=49.83333333333334 +lon_0=6.166666666666667 +k=1 +x_0=80000 +y_0=100000 +ellps=intl +towgs84=-189.681,18.3463,-42.7695,-0.33746,-3.09264,2.53861,0.4598 +units=m +no_defs');
 
 /**
  * @type {string}
