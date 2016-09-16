f131ff4d081ce1a3512e947f50ca5aa1aa212a9d
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2016-09-16 10:02:03 +0200

    Jsapi mymaps (#1426)
    * Add mymap support (features + style)
    
    * Mymaps popup with content
    
    * Add measure in mymaps popup
    
    * Fix closure issues
    
    * Show area for polygons
    
    * Adding circle radius & elevation
    
    * Style the content of the popup a bit
    
    * Get the popup to auto pan
    
    * Show profile when selecting a line
    
    * Show and hide the profile adequately
    
    * Make sure feature is unselected when closing popup
    
    * Add zoom to link
    
    * Change mymaps config options + fix build issues
    
    * Add support for callback on mymap load
    
    * Remove obsolete code
    
    * Export as CSV
    
    * Fix closure compiler errors
    
    * Add interaction profile/map
    
    * With translations
    
    * Translation should fail safely if no translation is available
    
    * Fix eslint check errors
    
    * Don't do things in MyMap constructor
    
    * More private methods
    
    * Fix dependencies to angular in api built file

---
+++ jsapi/examples/mymaps.js
@@ -0,0 +1,31 @@
+goog.provide('mymaps');
+
+goog.require('lux.Map');
+
+var map = new lux.Map({
+  target           : 'mapContainer',
+  position         : [ 6.13, 49.61 ],
+  positionSrs      : '4326',
+  zoom             : 14,
+  bgLayer          : 655
+});
+
+map.addMyMapLayer({
+  mapId: 'cf12fe433afa448bb6f3f47b4621127f',
+  profileTarget: 'profile'
+});
+
+var map = new lux.Map({
+  target           : 'mapContainer2',
+  position         : [ 6.13, 49.61 ],
+  positionSrs      : '4326',
+  zoom             : 14,
+  bgLayer          : 655
+});
+
+map.addMyMapLayer({
+  mapId: 'cf12fe433afa448bb6f3f47b4621127f',
+  onload: function(features) {
+    this.loadProfile(features[1].getGeometry(), 'profile2');
+  }
+});
