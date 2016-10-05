4b427e3ea9fd71422d1147003498f75ee107f857
rmichaelis <renaud.michaelis@gmail.com>
2016-10-05 14:40:11 +0200

    Migrate geocode from V2 to v3

--- geoportailv3/geocode.py
+++ geoportailv3/geocode.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import sqlahelper
 
-from sqlalchemy import Column, Unicode
+from sqlalchemy import Column, Unicode, Integer
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from geoalchemy2 import Geometry
@@ -24,3 +24,39 @@ class Address(Base):
     id_caclr_rue = Column(Unicode)
     id_caclr_bat = Column(Unicode)
     geom = Column(Geometry(srid=2169))
+
+
+class WKPOI(Base):
+    __table_args__ = ({'schema': 'geocoding', 'autoload': False})
+    __tablename__ = 'well_known_poi'
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode(255))
+    zip = Column(Integer)
+    locality = Column(Unicode(255))
+    street = Column(Unicode(255))
+    geom = Column(Geometry(srid=2169))
+
+    def dump(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'zip': self.zip,
+            'locality': self.locality,
+            'street': self.street,
+            'geom': self.geom}
+
+
+class Neighbourhood(Base):
+    __table_args__ = ({'schema': 'geocoding', 'autoload': False})
+    __tablename__ = 'neighbourhood'
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode(255))
+    locality = Column(Unicode(255))
+    geom = Column(Geometry(srid=2169))
+
+    def dump(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'locality': self.locality,
+            'geom': self.geom}
