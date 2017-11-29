eff2c68b9cd52624b6078272d47759ef064dd767
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2017-11-29 17:25:51 +0100

    Run ensure script in all situations

--- scripts/ensure_own_closure_util.sh
+++ scripts/ensure_own_closure_util.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+echo "Ensuring we use same version of closure-util"
+
 # We use a custom version of closure-util in our package.json.
 # We also use the build.js task of OpenLayers which hardcodes its closure-util version.
 # Ensure we use the same version everywhere.
-[ ! -d node_modules/openlayers/node_modules ] && npm install
+[ ! -d node_modules/openlayers/node_modules ] && echo "Calling again npm install" && npm install
 
 rm -rf node_modules/openlayers/node_modules/closure-util
 ln -s ../../@camptocamp/closure-util node_modules/openlayers/node_modules/closure-util
