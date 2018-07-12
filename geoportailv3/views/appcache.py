3bc812cc0c267f72c707c38ee3237d638521526b
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2018-07-12 08:40:15 +0200

    Fix flake8

--- geoportailv3/views/appcache.py
+++ geoportailv3/views/appcache.py
@@ -1,11 +1,11 @@
 from pyramid.view import view_config
-from pyramid.response import Response
-from pyramid.renderers import render
 import logging
 
 log = logging.getLogger(__name__)
 
-@view_config(route_name='appcache', http_cache=0, renderer='../templates/geoportailv3.appcache')
+
+@view_config(route_name='appcache', http_cache=0,
+             renderer='../templates/geoportailv3.appcache')
 def appcache(request):
     request.response.content_type = 'text/cache-manifest'
     return {}
