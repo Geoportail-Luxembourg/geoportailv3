8cc88f637cbd26dec2adda855e6b7ad0d69b4d8b
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2018-12-12 11:12:16 +0100

    Fix ngeo update

--- geoportal/webpack.config.js
+++ geoportal/webpack.config.js
@@ -2,21 +2,23 @@ const webpackMerge = require('webpack-merge');
 const apps = require('./webpack.apps.js');
 const commons = require('ngeo/buildtools/webpack.commons');
 
-let config = commons.config;
+let config = commons.config({}, false);
+let devProdConfig;
 
 const nodeEnv = process.env['NODE_ENV'] || 'development';
 switch (nodeEnv) {
   case 'development':
-    config = webpackMerge(config, require('ngeo/buildtools/webpack.dev'));
+    devProdConfig = require('ngeo/buildtools/webpack.dev');
     break;
   case 'production':
-    config = webpackMerge(config, require('ngeo/buildtools/webpack.prod'));
+    devProdConfig = require('ngeo/buildtools/webpack.prod');
     break;
   default:
     console.log(`The 'NODE_ENV' environment variable is set to an invalid value: ${process.env.NODE_ENV}.` );
     process.exit(2);
 }
 
-config = webpackMerge(config, apps);
+config = webpackMerge(config, apps, devProdConfig(false));
+
 
 module.exports = config;
