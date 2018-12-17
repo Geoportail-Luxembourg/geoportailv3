73560ed0a7d5922b0586ac32922d8560e8276d27
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2018-12-17 11:39:22 +0100

    Add the ping view

---
+++ geoportal/geoportailv3_geoportal/views/ping.py
@@ -0,0 +1,18 @@
+﻿# -*- coding: utf-8 -*-
+from pyramid.response import Response
+from pyramid.view import view_config
+from c2cgeoportal_geoportal.lib.caching import set_common_headers, NO_CACHE
+
+class Ping(object):
+
+    def __init__(self, request):
+        self.request = request
+
+    @view_config(route_name="ping")
+    def ping(self):
+        response = self.request.response
+        response.text = "pong"
+
+        return set_common_headers(
+            self.request, "ping", NO_CACHE
+        )
