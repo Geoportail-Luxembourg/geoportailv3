216e3520f9102d1572883a31b6168bf806dcf094
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2019-03-28 08:46:59 +0100

    Do not create a new user when fixing db

--- geoportal/fix-db.sh
+++ geoportal/fix-db.sh
@@ -3,7 +3,8 @@ alembic --name=main --config=/app/alembic.ini upgrade head;
 alembic --name=static --config=/app/alembic.ini upgrade head;
 alembic --name=lux --config=/app/alembic.ini upgrade head;
 finalize23DataAdaptations;
-manage-users --create -i production.ini -r role_admin -p c2c -e mail@c2c.ch admin
+
+#manage-users --create -i production.ini -r role_admin -p c2c -e mail@c2c.ch admin
 
 echo "These are layers appearing in several subclasses"
 psql <<EOF
