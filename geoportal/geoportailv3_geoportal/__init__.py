38335462a78f32731fca1d89f2bf74c2ad48ddcc
Renaud Michaëlis <renaud.michaelis@gmail.com>
2019-06-14 11:40:10 +0200

    migration poi proxy

--- geoportal/geoportailv3_geoportal/__init__.py
+++ geoportal/geoportailv3_geoportal/__init__.py
@@ -367,6 +367,7 @@ def main(global_config, **settings):
     config.add_route("echocsv", "/profile/echocsv", request_method="POST")
     config.add_route('getuserinfo', '/getuserinfo')
     config.add_route('wms', '/ogcproxywms')
+    config.add_route('wmspoi', '/wmspoi')
     config.add_route('https_proxy', '/httpsproxy')
     config.add_route('download_sketch', '/downloadsketch')
     config.add_route('download', '/download')
