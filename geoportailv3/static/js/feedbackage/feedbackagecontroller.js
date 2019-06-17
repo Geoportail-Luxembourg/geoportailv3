b56f95629ccfb183c3d195c28bbae4cbfbeeea42
rmichaelis <renaud.michaelis@gmail.com>
2019-06-17 14:28:59 +0200

    Change the title

--- geoportailv3/static/js/feedbackage/feedbackagecontroller.js
+++ geoportailv3/static/js/feedbackage/feedbackagecontroller.js
@@ -113,6 +113,13 @@ exports = function($scope, $http, appNotify, appUserManager,
    */
   this.url = '';
 
+  $scope.$on('gettextLanguageChanged', function() {
+    if (this['active'] && this.concernedLayerId == 0) {
+      this.concernedLayer =
+        this.gettextCatalog.getString('Please pick an AGE layer');
+    }
+  }.bind(this));
+
   $scope.$watch(goog.bind(function() {
     return this['active'];
   }, this), goog.bind(function(newVal) {
