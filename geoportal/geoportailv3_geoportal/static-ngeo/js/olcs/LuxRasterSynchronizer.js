9015dbb65cdad210622a49fe6b2cc524ecd95f9b
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2019-10-07 15:12:55 +0200

    Add support for default style in 3D

---
+++ geoportal/geoportailv3_geoportal/static-ngeo/js/olcs/LuxRasterSynchronizer.js
@@ -0,0 +1,22 @@
+import RasterSynchronizer from 'olcs/RasterSynchronizer';
+import MapBoxLayer from '@geoblocks/mapboxlayer-legacy';
+import {XYZ} from 'ol/source';
+import {Tile as TileLayer} from 'ol/layer';
+
+export default class LuxRasterSynchronizer extends RasterSynchronizer {
+
+  /**
+   * @override
+   */
+  convertLayerToCesiumImageries(olLayer, viewProj) {
+
+    if (olLayer instanceof MapBoxLayer && olLayer.getXYZ()) {
+      olLayer = new TileLayer({
+        source: new XYZ({
+          url: olLayer.getXYZ()
+        })
+      });
+    }
+    return super.convertLayerToCesiumImageries(olLayer, viewProj);
+  }
+}
