76bc4c61cb3d889b90b87686ea28015f98d06783
Renaud Michaelis <renaud.michaelis@gmail.com>
2020-04-06 08:16:28 +0200

    get the  metadata poc from the french language

--- geoportal/geoportailv3_geoportal/views/metadata.py
+++ geoportal/geoportailv3_geoportal/views/metadata.py
@@ -30,18 +30,35 @@ class Metadata(object):
             return HTTPBadRequest()
         base_url = os.environ["GEONETWORK_BASE_URL"]
         url = "{}/{}/q?_content_type=json&_isTemplate=y+or+n&_uuid_OR__id={}&fast=index".format(base_url, langs[lang], id)
+
         timeout = 15
         try:
             f = urllib.request.urlopen(url, None, timeout)
-            data = f.read()
+            data = json.loads(f.read())
         except Exception as e:
             log.exception(e)
             log.error(url)
             return HTTPBadGateway()
+        if lang != 'fr':
+            try:
+                url = "{}/{}/q?_content_type=json&_isTemplate=y+or+n&_uuid_OR__id={}&fast=index".format(base_url, langs["fr"], id)
+                f = urllib.request.urlopen(url, None, timeout)
+                data_fr = json.loads(f.read())
+            except Exception as e:
+                log.exception(e)
+                log.error(url)
+            if 'metadata' in data and 'responsibleParty' in data['metadata']:
+                if not isinstance(data['metadata']['responsibleParty'], type([])):
+                    data['metadata']['responsibleParty'] = [data['metadata']['responsibleParty']]
 
+            if 'metadata' in data_fr and 'responsibleParty' in data_fr['metadata']:
+                if isinstance(data_fr['metadata']['responsibleParty'], type([])):
+                    data['metadata']['responsibleParty'] = data['metadata']['responsibleParty'] + data_fr['metadata']['responsibleParty']
+                else:
+                    data['metadata']['responsibleParty'].append(data_fr['metadata']['responsibleParty'])
         if callback_param is None:
             headers = {"Content-Type": f.info()['Content-Type']}
             return Response(data, headers=headers)
 
         headers = {"Content-Type": "text/javascript"}
-        return Response("{}({})".format(callback_param, json.dumps(json.loads(data))), headers=headers)
+        return Response("{}({})".format(callback_param, json.dumps(data)), headers=headers)
