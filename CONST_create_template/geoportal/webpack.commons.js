b7afd932533ef7776adbcc2e181b834bdf62364d
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2020-10-26 13:27:46 +0100

    Add CONST_create_template

---
+++ CONST_create_template/geoportal/webpack.commons.js
@@ -0,0 +1,21 @@
+const commons = require('ngeo/buildtools/webpack.commons.js');
+const SassPlugin = require('ngeo/buildtools/webpack.plugin.js');
+
+const config = commons({
+  DllReferencePluginOptions: {
+    context: '/usr/lib/',
+  }
+});
+
+for (const plugin of config.plugins) {
+  if (plugin instanceof SassPlugin) {
+    plugin.options.preReplacements = [
+      [new RegExp('\\${VISIBLE_ENTRY_POINT}', 'g'), 'visible-entry-point'],
+    ];
+    plugin.options.postReplacements = [
+      [new RegExp('visible-entry-point', 'g'), '${VISIBLE_ENTRY_POINT}'],
+    ];
+  }
+}
+
+module.exports = () => config;
