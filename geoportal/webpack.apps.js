56e43a47477982cfe2d3b5a2d1f732c4b2992005
Antoine Abt <antoine@abt.im>
2021-03-16 15:12:18 +0100

    update build from 2.3

--- geoportal/webpack.apps.js
+++ geoportal/webpack.apps.js
@@ -1,6 +1,10 @@
 const path = require('path');
-const ls = require('ngeo/buildtools/ls.js');
+const ls = require('ls');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const INTERFACE_THEME = {"desktop":"desktop","desktop_alt":"desktop","mobile":"mobile","mobile_alt":"mobile","oeview":"desktop","oeedit":"desktop"};
 
 const plugins = [];
 const entry = {};
@@ -8,32 +12,31 @@ const entry = {};
 // The dev mode will be used for builds on local machine outside docker
 const nodeEnv = process.env['NODE_ENV'] || 'development';
 const dev = nodeEnv == 'development'
+process.traceDeprecation = true;
 
-for (const filename of ls(path.resolve(__dirname, 'geoportailv3_geoportal/static-ngeo/js/apps/*.html.ejs'))) {
-  const name = filename.file.substr(0, filename.file.length - '.html.ejs'.length);
-  entry[name] = 'geoportailv3/apps/' + name.charAt(0).toUpperCase() + name.slice(1) + 'Controller.js';
-  plugins.push(
-    new HtmlWebpackPlugin({
-      template: filename.full,
-      inject: false,
-      chunksSortMode: 'manual',
-      filename: name + '.html',
-      chunks: [name],
-      vars: {
-        entry_point: '${VISIBLE_ENTRY_POINT}',
-        version: '2.5.0.88',
-        cache_version: '${CACHE_VERSION}',
-       },
-    })
-  );
-}
+const name = 'main';
+process.env.THEME = INTERFACE_THEME[name];
 
-const babelPresets = [[require.resolve('@babel/preset-env'), {
-  targets: {
-    browsers: ['> 0.5% in CH', '> 0.5% in FR', 'Firefox ESR', 'ie 11'],
+entry[name] = 'geoportailv3/apps/MainController.js';
+plugins.push(
+  new HtmlWebpackPlugin({
+    inject: false,
+    template: path.resolve(__dirname, 'geoportailv3_geoportal/static-ngeo/js/apps/' + name + '.html.ejs'),
+    chunksSortMode: 'manual',
+    filename: name + '.html',
+    chunks: [name],
+    vars: {
+      entry_point: '${VISIBLE_ENTRY_POINT}',
+    },
+  })
+);
+
+const babelPresets = [['env',{
+  "targets": {
+    "browsers": ["last 2 versions", "Firefox ESR", "ie 11"],
   },
-  modules: false,
-  loose: true,
+  "modules": false,
+  "loose": true,
 }]]
 
 // Transform code to ES2015 and annotate injectable functions with an $inject array.
@@ -43,13 +46,7 @@ const projectRule = {
     loader: 'babel-loader',
     options: {
       presets: babelPresets,
-      babelrc: false,
-      comments: false,
-      plugins: [
-        require.resolve('@babel/plugin-syntax-object-rest-spread'),
-        require.resolve('@babel/plugin-transform-spread'),
-        require.resolve('@camptocamp/babel-plugin-angularjs-annotate'),
-      ],
+      plugins: ['@camptocamp/babel-plugin-angularjs-annotate'],
     }
   },
 };
@@ -59,21 +56,21 @@ const rules = [
 ];
 
 const noDevServer = process.env['NO_DEV_SERVER'] == 'TRUE';
-const devServer = dev && !noDevServer;
+devServer = dev && !noDevServer;
 
 console.log("Use dev mode: " + dev)
 console.log("Use dev server mode: " + devServer)
 
+rules.push({
+  test: /\.js$/,
+  use: ["source-map-loader"],
+  enforce: "pre"
+});
+
 module.exports = {
   output: {
-    path: '/etc/static-ngeo/',
-    publicPath: devServer ? '${VISIBLE_ENTRY_POINT}dev/' : '.__ENTRY_POINT__static-ngeo/'
-  },
-  devServer: {
-    publicPath: '${VISIBLE_WEB_PROTOCOL}://${VISIBLE_WEB_HOST}${VISIBLE_ENTRY_POINT}dev/',
-    port: 8080,
-    host: 'webpack_dev_server',
-    hot: true,
+    path: path.resolve(__dirname, 'geoportailv3_geoportal/static-ngeo/build/'),
+    publicPath: devServer ? '${VISIBLE_ENTRY_POINT}dev/' : '${VISIBLE_ENTRY_POINT}static-ngeo/UNUSED_CACHE_VERSION/build/'
   },
   entry: entry,
   module: {
@@ -84,6 +81,12 @@ module.exports = {
     modules: ['/usr/lib/node_modules'],
     alias: {
       geoportailv3: path.resolve(__dirname, 'geoportailv3_geoportal/static-ngeo/js'),
-    },
+      'jsts': '/usr/lib/node_modules/jsts/org/locationtech/jts',
+      'ol/ol.css': '/usr/lib/node_modules/openlayers/css/ol.css',
+      'ol': '/usr/lib/node_modules/openlayers/src/ol',
+      'olcs': '/usr/lib/node_modules/ol-cesium/src/olcs',
+      'jquery-ui/datepicker': '/usr/lib/node_modules/jquery-ui/ui/widgets/datepicker', // For angular-ui-date
+      'proj4': '/usr/lib/node_modules/proj4/lib',
+    }
   },
 };
