86a6f65cf48b18ed1a6b496c097d84d22865a74b
Renaud Michaelis <renaud.michaelis@gmail.com>
2021-09-01 17:13:31 +0200

    Remove artifact on background for mvt topo layers

--- geoportal/geoportailv3_geoportal/static-ngeo/ngeo/src/map/BackgroundLayerMgr.js
+++ geoportal/geoportailv3_geoportal/static-ngeo/ngeo/src/map/BackgroundLayerMgr.js
@@ -103,7 +103,7 @@ export default class BackgroundLayerMgr extends olObservable {
    */
   set(map, layer) {
     const ZIndex = -200;
-    map.getTargetElement().classList.toggle('blankBackground', layer.get('role') === 'blank' || layer.get('role') === 'mapboxBackground');
+    map.getTargetElement().classList.toggle('blankBackground', layer.get('role') === 'blank' || layer.get('role') === 'mapboxBackground' || layer.get('role') === undefined);
     const mapUid = olBase.getUid(map).toString();
     const previous = this.get(map);
     if (layer !== null) {
