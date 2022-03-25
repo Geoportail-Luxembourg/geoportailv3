7c0a8a0096a8325a1504c91c1205b94fc5bc0e89
Tobias Kohr <tobias.kohr@camptocamp.com>
2022-03-25 13:45:17 +0100

    fix(dev): proxy api dev server to compo for resources

--- geoportal/jsapi/web-dev-server.config.mjs
+++ geoportal/jsapi/web-dev-server.config.mjs
@@ -1,9 +1,17 @@
 import {fromRollup} from '@web/dev-server-rollup';
 import rollupCommonjs from '@rollup/plugin-commonjs';
+import proxy from 'koa-proxies';
 
 const commonjs = fromRollup(rollupCommonjs);
 
 export default {
+  //redirect to use apiv4loader, static ressources and APIs from docker compo
+  port: 8000,
+  middleware: [
+    proxy(['/apiv4loader.js', '/static-ngeo/', '/mymaps/', '/geocode/', /^\/fulltextsearch?/], {
+      target: 'http://localhost:8080/',
+    }),
+  ],
   plugins: [
     commonjs({
       include: [
