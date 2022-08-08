cca09dfe48e632d3ed2538969d1493a1383ac0f2
Moritz Kirmse <moritz.kirmse@camptocamp.com>
2022-08-08 22:54:05 +0200

    add layer times into local storage state and permalink

--- geoportal/geoportailv3_geoportal/static-ngeo/js/layermanager/LayermanagerController.js
+++ geoportal/geoportailv3_geoportal/static-ngeo/js/layermanager/LayermanagerController.js
@@ -27,9 +27,11 @@ class Controller {
    * @param {angular.$rootScope} $rootScope Angular rootScope service.
    * @ngInject
    */
-  constructor(ngeoLocation, ngeoBackgroundLayerMgr, $rootScope) {
+  constructor(ngeoLocation, ngeoBackgroundLayerMgr, appTimeLayer, $rootScope) {
 
     this.ngeoLocation_ = ngeoLocation;
+
+    this.timeLayer_ = appTimeLayer
 
     this.ngeoBackgroundLayerMgr_ = ngeoBackgroundLayerMgr;
 
@@ -79,29 +81,16 @@ class Controller {
   }
 
   setTime(layer, time) {
-    if (layer.type == 'IMAGE') {
-      let dd = new Date(time.start)
-      let pp = layer.getSource().getParams();
-      pp['TIME'] = dd.toISOString();
-      if (time.end != undefined) {
-        dd = new Date(time.end)
-        pp['TIME'] += '/' + dd.toISOString();
-      }
-      layer.getSource().updateParams(pp);
+    let dd = new Date(time.start);
+    // remove milleseconds
+    let timeString = dd.toISOString().split('.')[0]+"Z";
+    if (time.end != undefined) {
+      dd = new Date(time.end)
+      // remove milleseconds
+      timeString += '/' + dd.toISOString().split('.')[0]+"Z";
     }
-    else if (layer.type == 'TILE') {
-      let WMTS_source = layer.getSource();
-      let isWmts = WMTS_source instanceof olSourceWMTS;
-      if (isWmts) {
-        let dd = new Date(time.start);
-        let oldLayer = WMTS_source.getLayer();
-        let newLayer = layer.get('metadata')['time_layers'][dd.toISOString().split('.')[0]+"Z"];
-        let oldUrls = WMTS_source.getUrls();
-        let newUrls = oldUrls.map((url) => url.replace(/\/[^\/]*\/{TileMatrixSet}/, '/' + newLayer + '/{TileMatrixSet}'))
-        WMTS_source.setUrls(newUrls);
-        layer.set('label', newLayer);
-      }
-    }
+    this.timeLayer_.setTime(layer, timeString);
+    layer.set('current_time', timeString)
   }
 
   reorderCallback3D(element, layers) {
@@ -155,6 +144,29 @@ class Controller {
   }
 };
 
+class TimeLayer {
+    setTime(layer, time) {
+    if (layer.type == 'IMAGE') {
+      let pp = layer.getSource().getParams();
+      pp['TIME'] = time;
+      layer.getSource().updateParams(pp);
+    }
+    else if (layer.type == 'TILE') {
+      let WMTS_source = layer.getSource();
+      let isWmts = WMTS_source instanceof olSourceWMTS;
+      if (isWmts) {
+        let oldLayer = WMTS_source.getLayer();
+        let newLayer = layer.get('metadata')['time_layers'][time];
+        let oldUrls = WMTS_source.getUrls();
+        let newUrls = oldUrls.map((url) => url.replace(/\/[^\/]*\/{TileMatrixSet}/, '/' + newLayer + '/{TileMatrixSet}'))
+        WMTS_source.setUrls(newUrls);
+        layer.set('label', newLayer);
+      }
+    }
+  }
+};
+
 appModule.controller('AppLayermanagerController', Controller);
+appModule.service('appTimeLayer', TimeLayer);
 
 export default Controller;
