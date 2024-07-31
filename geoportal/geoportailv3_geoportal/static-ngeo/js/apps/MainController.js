8f6e056d5809d2ed8b25bd17a807d3ba8fe3b5e4
Moritz Kirmse <61978301+mki-c2c@users.noreply.github.com>
2024-07-31 07:50:07 +0200

    Compare only layer ids and not layer in cache

--- geoportal/geoportailv3_geoportal/static-ngeo/js/apps/MainController.js
+++ geoportal/geoportailv3_geoportal/static-ngeo/js/apps/MainController.js
@@ -1801,7 +1801,7 @@ MainController.prototype.manageSelectedLayers_ =
           if (layer instanceof MaskLayer) {
             return false;
           }
-          if (useOpenLayers().getLayerFromCache(this.mapStore_.bgLayer) === layer) {
+          if (this.mapStore_.bgLayer.id === layer.get('id')) {
             return false;
           }
           // Ignore layer for measurements
